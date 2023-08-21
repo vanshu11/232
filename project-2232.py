b208a6f6c0c4d6441cf80b0e031493e22bf62b24
vanshu11 <87985098+vanshu11@users.noreply.github.com>
2023-08-21 18:54:37 +0530

    Add files via upload

---
+++ project-2232.py
@@ -0,0 +1,17 @@
+from numpy import loadtxt
+from keras.models import Sequential
+from keras.layers import Dense
+dataset=loadtxt('pokemon.csv',delimiter=',')
+x=dataset[:,1:7]
+y=dataset[:,8]
+print("value of x are:",x)
+print("value of y are:",y)
+model=Sequential()
+model.add(Dense(12,input_dim=6,activation='relu'))
+model.add(Dense(8,activation='relu'))
+model.add(Dense(1,activation='sigmoid'))
+model.compile(loss='binary_crossentropy',metrics=['accuracy'])
+model.fit(x,y,epochs=250,batch_size=100)
+predictions=model.predict_classes(x)
+for i in range(785,800):
+	print(f'{x[i].tolist()}=>{predictions[i]}expected{y[i]}')
